87190c2e5bc3ebc6a6eebfea2ab71d673a4ddda9
Patricia Della Matta <patricia_dellamatta@hotmail.com>
2020-09-22 17:57:01 -0300

    Entidade TodoItem

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todo.Domain.Entities
+{
+    public abstract class Entity : IEquatable<Entity> //Permite executar comparações
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        public bool Equals(Entity other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
