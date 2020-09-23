0b3bd6e1e528b8c32a745e310b80f4e42a9e7cbd
Patricia Della Matta <patricia_dellamatta@hotmail.com>
2020-09-23 10:12:40 -0300

    Definindo o Contrato dos Comandos

--- Todo.Domain/Commands/Contracts/ICommand.cs
+++ Todo.Domain/Commands/Contracts/ICommand.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Flunt.Validations;
 
 namespace Todo.Domain.Commands.Contracts
 {
-    public interface ICommand
+    public interface ICommand : IValidatable
     {
-        bool Validate();
+
     }
 }
