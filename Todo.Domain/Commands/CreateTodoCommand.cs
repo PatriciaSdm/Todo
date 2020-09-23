e056d6a293c40e87d196d98e5ec2da8a6d9d9a5f
Patricia Della Matta <patricia_dellamatta@hotmail.com>
2020-09-23 10:21:37 -0300

    Estrutura dos commands

---
+++ Todo.Domain/Commands/CreateTodoCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Todo.Domain.Commands.Contracts;
+
+namespace Todo.Domain.Commands
+{
+    public class CreateTodoCommand : ICommand
+    {
+        public CreateTodoCommand() { }
+
+        public CreateTodoCommand(string title, string user, DateTime date)
+        {
+            Title = title;
+            Date = date;
+            User = user;
+        }
+
+        public string Title { get; private set; }
+        public DateTime Date { get; private set; }
+        public string User { get; private set; }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
