3a55b41375a75f0995162724e67403c92a361fa5
Patricia Della Matta <patricia_dellamatta@hotmail.com>
2020-09-24 10:39:02 -0300

    Finalizando o Handler

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -13,7 +13,9 @@ namespace Todo.Domain.Handlers
     public class TodoHandler :
         Notifiable,
         IHandler<CreateTodoCommand>,
-        IHandler<UpdateTodoCommand>
+        IHandler<UpdateTodoCommand>,
+        IHandler<MarkTodoAsDoneCommand>,
+        IHandler<MarkTodoAsUndoneCommand>
     {
         private readonly ITodoRepository _repository;
 
@@ -59,6 +61,46 @@ namespace Todo.Domain.Handlers
             return new GenericCommandResult(true, "Tarefa salva", todo);
 
 
+        }
+
+        public ICommandResult Handle(MarkTodoAsDoneCommand command)
+        {
+            //Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que a sua tarefa esta errada!", command.Notifications);
+
+            // Recupera o TodoItem (Rehidratação)
+            var todo = _repository.GetById(command.Id, command.User);
+
+            // Altera o estado
+            todo.MarkAsDone();
+
+            // Salva no banco
+            _repository.Update(todo);
+
+            //Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa salva", todo);
+        }
+
+        public ICommandResult Handle(MarkTodoAsUndoneCommand command)
+        {
+            //Fail Fast Validation
+            command.Validate();
+            if (command.Invalid)
+                return new GenericCommandResult(false, "Ops, parece que a sua tarefa esta errada!", command.Notifications);
+
+            // Recupera o TodoItem (Rehidratação)
+            var todo = _repository.GetById(command.Id, command.User);
+
+            // Altera o estado
+            todo.MarkAsUndone();
+
+            // Salva no banco
+            _repository.Update(todo);
+
+            //Retorna o resultado
+            return new GenericCommandResult(true, "Tarefa salva", todo);
         }
     }
 }
