e18a147c647f76ea7ac173dfcec994e9644955a0
Patricia Della Matta <patricia_dellamatta@hotmail.com>
2020-09-25 16:04:10 -0300

    Finalizando o Controller

--- Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -17,18 +17,79 @@ namespace Todo.Domain.Api.Controllers
 
         [Route("")]
         [HttpGet]
-        public IEnumerable<TodoItem> GetAll([FromServices]ITodoRepository repository)
+        public IEnumerable<TodoItem> GetAll([FromServices] ITodoRepository repository)
         {
             return repository.GetAll("patriciamatta");
         }
 
+
+        [Route("done")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllDone([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllDone("patriciamatta");
+        }
+
+        [Route("undone")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllUndone([FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllUndone("patriciamatta");
+        }
+
+
+        [Route("done/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForToday([FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod("patriciamatta", DateTime.Now.Date, true);
+        }
+
+
+        [Route("undone/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForToday([FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod("patriciamatta", DateTime.Now.Date, false);
+        }
+
+        [Route("done/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForTomorrow([FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod("patriciamatta", DateTime.Now.Date.AddDays(1), true);
+        }
+
+        [Route("undone/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForTomorrow([FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod("patriciamatta", DateTime.Now.Date.AddDays(1), false);
+        }
+
         [Route("")]
         [HttpPost]
-        public GenericCommandResult Create([FromBody]CreateTodoCommand command, [FromServices]TodoHandler handler) //FromServices vem do startup
+        public GenericCommandResult Create([FromBody] CreateTodoCommand command, [FromServices] TodoHandler handler) //FromServices vem do startup
         {
             command.User = "patriciamatta";
             return (GenericCommandResult)handler.Handle(command);
 
+        }
+
+        [Route("")]
+        [HttpPost]
+        public GenericCommandResult Update([FromBody] UpdateTodoCommand command, [FromServices] TodoHandler handler) //FromServices vem do startup
+        {
+            command.User = "patriciamatta";
+            return (GenericCommandResult)handler.Handle(command);
+        }
+
+        [Route("")]
+        [HttpPost]
+        public GenericCommandResult MarkAsDone([FromBody] MarkTodoAsDoneCommand command, [FromServices] TodoHandler handler) //FromServices vem do startup
+        {
+            command.User = "patriciamatta";
+            return (GenericCommandResult)handler.Handle(command);
         }
     }
 }
